9fef95e771b6cceeb4377fd85e9a0ff8b85bdb41
ardy <afalls@qvxlabs.com>
2016-06-30 02:05:56 -0700

    Fixed bugs in the test cases so that they now all pass all the time

--- tests/unit-tests.c
+++ tests/unit-tests.c
@@ -11,10 +11,26 @@
 /* ------------ */
 #define OUTPUT_FILE "unittest"
 /* ------------ */
+typedef enum {
+  TSTTASK_INIT = 0,
+  TSTTASK_RUN,
+  TSTTASK_CANCEL,
+  TSTTASK_DONE,
+  TSTTASK_MAX
+} test_state;
+/* ------------ */
 typedef struct {
+  test_state m_state;
+  pthread_mutex_t m_runLock;
+  pthread_mutex_t m_cancelLock;
+  pthread_mutex_t m_endLock;
+  pthread_cond_t m_runCond;
+  pthread_cond_t m_cancelCond;
+  pthread_cond_t m_endCond;
   int m_run;
   int m_end;
-} test_context;
+  int m_cancel;
+  } test_context;
 /* ------------ */
 static int _runTests(const char *);
 static void _paramTest_01(void);
@@ -43,6 +59,10 @@ static void _funcTest_14(void);
 static void _funcTest_15(void);
 static void _funcTest_16(void);
 static void _funcTest_17(void);
+static void _taskContextInit(test_context *);
+static void _taskContextDestroy(test_context *);
+static void _taskContextSignalRun(test_context *);
+static void _taskContextWaitCancel(test_context *);
 static void * _taskRun(dispatch_task, void *);
 static void _taskFin(dispatch_task, void *, void *);
 static void _taskCnl(dispatch_task, void *);
@@ -82,10 +102,6 @@ static CU_TestInfo funct_tests[] = {
   { "Functionality Test 17", _funcTest_17 },  
   CU_TEST_INFO_NULL
 };
-/* ------------ */
-static int gCanceled;
-static pthread_mutex_t gCancelLock;
-static pthread_cond_t gCancelCond;
 /* ------------ */
 static CU_SuiteInfo suites[] = {
   {
@@ -163,59 +179,48 @@ _paramTest_07(void) {
 /* ------------ */
 static void
 _func_test_setUp(void) {
-  gCanceled = 0;
-  pthread_mutex_init(&gCancelLock, NULL);
-  pthread_cond_init(&gCancelCond, NULL);  
+  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
 }
 /* ------------ */
 static void
 _func_test_tearDown(void) {
-  pthread_mutex_destroy(&gCancelLock);
-  pthread_cond_destroy(&gCancelCond);  
+  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
 }
 /* ------------ */
 static void
 _funcTest_01(void) {
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
+  /* does nothing */
 }
 /* ------------ */
 static void
 _funcTest_02(void) {
   dispatch_task pTask;
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
   CU_ASSERT(NULL != (pTask = dispatch_init(_taskRun, _taskCnl, NULL)));
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
 }
 /* ------------ */
 static void
 _funcTest_03(void) {
   dispatch_task pTask;
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
   CU_ASSERT(NULL != (pTask = dispatch_initEx(_taskRun,
 					     _taskCnl,
 					     _taskFin,
 					     NULL)));
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
 }
 /* ------------ */
 static void
 _funcTest_04(void) {
   dispatch_task pTask;
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
   CU_ASSERT(NULL != (pTask = dispatch_init(_taskRun, _taskCnl, NULL)));
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
   CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
 }
 /* ------------ */
 static void
 _funcTest_05(void) {
   dispatch_task pTask;
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
   CU_ASSERT(NULL != (pTask = dispatch_initEx(_taskRun,
 					     _taskCnl,
 					     _taskFin,
@@ -223,67 +228,84 @@ _funcTest_05(void) {
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
   CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
 }
 /* ------------ */
 static void
 _funcTest_06(void) {
   dispatch_task pTask;
-  test_context ctx = { 0, 0 };
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
+  test_context ctx;
+  /* init task */
+  _taskContextInit(&ctx);
   CU_ASSERT(NULL != (pTask = dispatch_init(_taskRun, _taskCnl, &ctx)));
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
+  /* signal task to run */
+  _taskContextSignalRun(&ctx);
+  /* wait for completion */
   CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
+  /* verify results */
   CU_ASSERT(&ctx == dispatch_return(pTask));
   CU_ASSERT(1 == ctx.m_run);
+  CU_ASSERT(TSTTASK_RUN == ctx.m_state);
+  /* destroy task */
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
+  _taskContextDestroy(&ctx);
 }
 /* ------------ */
 static void
 _funcTest_07(void) {
   dispatch_task pTask;
-  test_context ctx = { 0, 0 };
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
+  test_context ctx;
+  /* init task */
+  _taskContextInit(&ctx);
   CU_ASSERT(NULL != (pTask = dispatch_initEx(_taskRun,
 					     _taskCnl,
 					     _taskFin,
 					     &ctx)));
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
+  /* signal task to run */
+  _taskContextSignalRun(&ctx);  
+  /* wait for completion */  
   CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
   CU_ASSERT(&ctx == dispatch_return(pTask));
+  /* verify results */  
   CU_ASSERT(1 == ctx.m_run);
   CU_ASSERT(1 == ctx.m_end);
+  CU_ASSERT(TSTTASK_DONE == ctx.m_state);
+  /* destroy task */  
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
+  _taskContextDestroy(&ctx);
 }
 /* ------------ */
 static void
 _funcTest_08(void) {
   int ndx;
-  dispatch_task * ppTask;
-  test_context * pCtx;
   const int nTasks = (int)sysconf(_SC_NPROCESSORS_ONLN) * 2;
-  /* allocate memory for test */
-  ppTask = malloc(sizeof(dispatch_task) * nTasks);
-  pCtx = malloc(sizeof(test_context) * nTasks);
-  memset(pCtx, 0, sizeof(test_context) * nTasks);
-  /* run tests */
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
+  dispatch_task * ppTask = malloc(sizeof(dispatch_task) * nTasks);
+  test_context * pCtx = malloc(sizeof(test_context) * nTasks);
+  /* init tasks */
   for (ndx = 0; ndx < nTasks; ++ndx) {
+    _taskContextInit(&pCtx[ndx]);
     CU_ASSERT(NULL != (ppTask[ndx] = dispatch_init(_taskRun,
 						   _taskCnl,
-						   &pCtx[ndx])));
-    CU_ASSERT(DISPATCH_OK == dispatch_schedule(ppTask[ndx]));
+						   &pCtx[ndx])));    
   }
+  /* run tasks */
+  for (ndx = 0; ndx < nTasks; ++ndx) {
+    CU_ASSERT(DISPATCH_OK == dispatch_schedule(ppTask[ndx]));
+    _taskContextSignalRun(&pCtx[ndx]);
+  }  
   CU_ASSERT(DISPATCH_OK == dispatch_wait(ppTask, nTasks));
+  /* verify results */  
   for (ndx = 0; ndx < nTasks; ++ndx) {
     CU_ASSERT(&pCtx[ndx] == dispatch_return(ppTask[ndx]));
     CU_ASSERT(1 == pCtx[ndx].m_run);
-    CU_ASSERT(DISPATCH_OK == dispatch_destroy(ppTask[ndx]));
+    CU_ASSERT(TSTTASK_RUN == pCtx[ndx].m_state);
   }
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
-  /* free test memory */
+  /* destroy tasks */
+  for (ndx = 0; ndx < nTasks; ++ndx) {
+    CU_ASSERT(DISPATCH_OK == dispatch_destroy(ppTask[ndx]));
+    _taskContextDestroy(&pCtx[ndx]);
+  }
   free(ppTask);
   free(pCtx);
 }
@@ -291,31 +313,35 @@ _funcTest_08(void) {
 static void
 _funcTest_09(void) {
   int ndx;
-  dispatch_task * ppTask;
-  test_context * pCtx;
   const int nTasks = (int)sysconf(_SC_NPROCESSORS_ONLN) * 2;
-  /* allocate memory for test */
-  ppTask = malloc(sizeof(dispatch_task) * nTasks);
-  pCtx = malloc(sizeof(test_context) * nTasks);
-  memset(pCtx, 0, sizeof(test_context) * nTasks);
-  /* run tests */
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
+  dispatch_task * ppTask = malloc(sizeof(dispatch_task) * nTasks);
+  test_context * pCtx = malloc(sizeof(test_context) * nTasks);
+  /* init tasks */
   for (ndx = 0; ndx < nTasks; ++ndx) {
+    _taskContextInit(&pCtx[ndx]);
     CU_ASSERT(NULL != (ppTask[ndx] =
 		       dispatch_initEx(_taskRun,
 				       _taskCnl,
 				       _taskFin, &pCtx[ndx])));
+  }  
+  /* run tasks */
+  for (ndx = 0; ndx < nTasks; ++ndx) {
     CU_ASSERT(DISPATCH_OK == dispatch_schedule(ppTask[ndx]));
-  }
+    _taskContextSignalRun(&pCtx[ndx]);
+  }  
   CU_ASSERT(DISPATCH_OK == dispatch_wait(ppTask, nTasks));
+  /* verify results */
   for (ndx = 0; ndx < nTasks; ++ndx) {
     CU_ASSERT(&pCtx[ndx] == dispatch_return(ppTask[ndx]));
     CU_ASSERT(1 == pCtx[ndx].m_run);
     CU_ASSERT(1 == pCtx[ndx].m_end);
-    CU_ASSERT(DISPATCH_OK == dispatch_destroy(ppTask[ndx]));
+    CU_ASSERT(TSTTASK_DONE == pCtx[ndx].m_state);
   }
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
-  /* free test memory */
+  /* destroy tasks */
+  for (ndx = 0; ndx < nTasks; ++ndx) {
+    CU_ASSERT(DISPATCH_OK == dispatch_destroy(ppTask[ndx]));
+    _taskContextDestroy(&pCtx[ndx]);
+  }
   free(ppTask);
   free(pCtx);
 }
@@ -323,145 +349,229 @@ _funcTest_09(void) {
 static void
 _funcTest_10(void) {
   dispatch_task pTask;
-  test_context ctx = { 0, 0 };
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
+  test_context ctx;
+  /* init task */
+  _taskContextInit(&ctx);
   CU_ASSERT(NULL != (pTask = dispatch_init(_taskRun, _taskCnl, &ctx)));
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
+  /* signal task to run */  
+  _taskContextSignalRun(&ctx);
+  /* wait for completion */  
   CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
+  /* verify results */  
   CU_ASSERT(&ctx == dispatch_return(pTask));
   CU_ASSERT(1 == ctx.m_run);
+  CU_ASSERT(TSTTASK_RUN == ctx.m_state);
+  /* reschedule task */
+  ctx.m_state = TSTTASK_INIT;
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
+  _taskContextSignalRun(&ctx);
+  /* wait for completion */
   CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
+  /* verify results */
   CU_ASSERT(&ctx == dispatch_return(pTask));
-  CU_ASSERT(2 == ctx.m_run);  
+  CU_ASSERT(2 == ctx.m_run);
+  CU_ASSERT(TSTTASK_RUN == ctx.m_state);
+  /* destroy task */
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
+  _taskContextDestroy(&ctx);
 }
 /* ------------ */
 static void
 _funcTest_11(void) {
   dispatch_task pTask;
-  test_context ctx = { 0, 0 };
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
+  test_context ctx;
+  /* init task */
+  _taskContextInit(&ctx);
   CU_ASSERT(NULL != (pTask = dispatch_initEx(_taskRun,
 					     _taskCnl,
 					     _taskFin,
-					     &ctx)));
+					     &ctx))); 
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
+  /* signal task to run */  
+  _taskContextSignalRun(&ctx);
+  /* wait for completion */  
   CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
+  /* verify results */  
   CU_ASSERT(&ctx == dispatch_return(pTask));
   CU_ASSERT(1 == ctx.m_run);
   CU_ASSERT(1 == ctx.m_end);
+  CU_ASSERT(TSTTASK_DONE == ctx.m_state);
+  /* reschedule task */
+  ctx.m_state = TSTTASK_INIT;
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
+  _taskContextSignalRun(&ctx);
+  /* wait for completion */
   CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
+  /* verify results */
   CU_ASSERT(&ctx == dispatch_return(pTask));
   CU_ASSERT(2 == ctx.m_run);
   CU_ASSERT(2 == ctx.m_end);
+  CU_ASSERT(TSTTASK_DONE == ctx.m_state);  
+  /* destroy task */
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
+  _taskContextDestroy(&ctx);
 }
 /* ------------ */
 static void
 _funcTest_12(void) {
   dispatch_task pTask;
-  test_context ctx = { 0, 0 };
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
+  test_context ctx;
+  /* init task */
+  _taskContextInit(&ctx);
   CU_ASSERT(NULL != (pTask = dispatch_initEx(_taskRunReschedule,
 					     _taskCnl,
 					     _taskFinReschedule,
 					     &ctx)));
+  /* run task */
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
+  _taskContextSignalRun(&ctx);  
   CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
+  /* verify results */
   CU_ASSERT(&ctx == dispatch_return(pTask));
   CU_ASSERT(1 == ctx.m_run);
   CU_ASSERT(1 == ctx.m_end);
+  CU_ASSERT(TSTTASK_DONE == ctx.m_state);
+  /* destroy task */
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
+  _taskContextDestroy(&ctx);  
 }
 /* ------------ */
 static void
 _funcTest_13(void) {
   dispatch_task pTask;
-  test_context ctx = { 0, 0 };
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
+  test_context ctx;
+  /* init task */
+  _taskContextInit(&ctx);
   CU_ASSERT(NULL != (pTask = dispatch_initEx(_taskRunWait,
 					     _taskCnl,
 					     _taskFinWait,
 					     &ctx)));
+  /* run task */  
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
+  _taskContextSignalRun(&ctx);  
   CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
+  /* verify results */
   CU_ASSERT(&ctx == dispatch_return(pTask));
   CU_ASSERT(1 == ctx.m_run);
   CU_ASSERT(1 == ctx.m_end);
+  CU_ASSERT(TSTTASK_DONE == ctx.m_state);
+  /* destroy task */
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
+  _taskContextDestroy(&ctx);
 }
 /* ------------ */
 static void
 _funcTest_14(void) {
   dispatch_task pTask;
-  test_context ctx = { 0, 0 };
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
+  test_context ctx;
+  /* init task */
+  _taskContextInit(&ctx);
   CU_ASSERT(NULL != (pTask = dispatch_initEx(_taskRun,
 					     _taskCnl,
 					     _taskFin,
 					     &ctx)));
+  /* run task */    
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
-  pthread_yield();
+  pthread_yield();  /* wait for task to hit lock */
   dispatch_cancel(pTask);
-  pthread_yield();  
-  CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
-  pthread_yield();
-  pthread_mutex_lock(&gCancelLock);
-  while (!gCanceled && !ctx.m_end) {
-    pthread_cond_wait(&gCancelCond, &gCancelLock);
-  }
-  pthread_mutex_unlock(&gCancelLock); 
+  _taskContextSignalRun(&ctx);  
+  _taskContextWaitCancel(&ctx);
+  /* destroy task */  
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));  
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
+  _taskContextDestroy(&ctx);
 }
 /* ------------ */
 static void
 _funcTest_15(void) {
   dispatch_task pTask;
-  test_context ctx = { 0, 0 };
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
-  CU_ASSERT(NULL != (pTask = dispatch_init(_taskRun, _taskCnl, &ctx)));
+  CU_ASSERT(NULL != (pTask = dispatch_init(_taskRun, _taskCnl, NULL)));
   CU_ASSERT(DISPATCH_INVALID_OPERATION == dispatch_cancel(pTask));
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
 }
 /* ------------ */
 static void
 _funcTest_16(void) {
   dispatch_task pTask;
-  test_context ctx = { 0, 0 };
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
-  CU_ASSERT(NULL != (pTask = dispatch_init(_taskRun, _taskCnl, &ctx)));
+  CU_ASSERT(NULL != (pTask = dispatch_init(_taskRun, _taskCnl, NULL)));
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
   CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
   CU_ASSERT(DISPATCH_INVALID_OPERATION == dispatch_cancel(pTask));  
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
 }
 /* ------------ */
 static void
 _funcTest_17(void) {
   dispatch_task pTask;
-  test_context ctx = { 0, 0 };
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_init());
+  test_context ctx;
+  /* init task */
+  _taskContextInit(&ctx);  
   CU_ASSERT(NULL != (pTask = dispatch_init(_taskRun, _taskCnl, &ctx)));
+  /* signal task to run */  
   CU_ASSERT(DISPATCH_OK == dispatch_schedule(pTask));
-  CU_ASSERT(DISPATCH_INVALID_OPERATION == dispatch_destroy(pTask));  
+  CU_ASSERT(DISPATCH_INVALID_OPERATION == dispatch_destroy(pTask));
+  _taskContextSignalRun(&ctx);  
   CU_ASSERT(DISPATCH_OK == dispatch_wait(&pTask, 1));
+  /* destroy task */  
   CU_ASSERT(DISPATCH_OK == dispatch_destroy(pTask));
-  CU_ASSERT(DISPATCH_OK == dispatch_engine_destroy());
+  _taskContextDestroy(&ctx);
+}
+/* ------------ */
+static void
+_taskContextInit(test_context * pCtx) {
+  test_context init = {
+    TSTTASK_INIT,
+    PTHREAD_MUTEX_INITIALIZER,
+    PTHREAD_MUTEX_INITIALIZER,
+    PTHREAD_MUTEX_INITIALIZER,
+    PTHREAD_COND_INITIALIZER,
+    PTHREAD_COND_INITIALIZER,
+    PTHREAD_COND_INITIALIZER,    
+    0,
+    0,
+    0
+  };
+  *pCtx = init;
+}
+/* ------------ */
+static void
+_taskContextDestroy(test_context * pCtx) {
+  pthread_mutex_destroy(&pCtx->m_runLock);
+  pthread_mutex_destroy(&pCtx->m_cancelLock);
+  pthread_mutex_destroy(&pCtx->m_endLock);
+  pthread_cond_destroy(&pCtx->m_runCond);
+  pthread_cond_destroy(&pCtx->m_cancelCond);
+  pthread_cond_destroy(&pCtx->m_endCond);
+}
+/* ------------ */
+static void
+_taskContextSignalRun(test_context * pCtx) {
+  pthread_mutex_lock(&pCtx->m_runLock);
+  pCtx->m_state = TSTTASK_RUN;
+  pthread_cond_signal(&pCtx->m_runCond);
+  pthread_mutex_unlock(&pCtx->m_runLock);
+}
+/* ------------ */
+static void
+_taskContextWaitCancel(test_context * pCtx) {
+  pthread_mutex_lock(&pCtx->m_cancelLock);
+  while (TSTTASK_CANCEL != pCtx->m_state) {
+    pthread_cond_wait(&pCtx->m_cancelCond, &pCtx->m_cancelLock);
+  }
+  pthread_mutex_unlock(&pCtx->m_cancelLock);
 }
 /* ------------ */
 static void *
 _taskRun(dispatch_task pTask, void * pContext) {
   test_context * pCtx = (test_context *)pContext;
-  if (pCtx) {
+  if (pCtx) {    
+    /* wait for run signal */
+    pthread_mutex_lock(&pCtx->m_runLock);
+    while (TSTTASK_RUN != pCtx->m_state) {
+      pthread_cond_wait(&pCtx->m_runCond, &pCtx->m_runLock);
+    }
+    pthread_mutex_unlock(&pCtx->m_runLock);
+    /* update run counter */    
     pCtx->m_run++;
   }    
   return pCtx;
@@ -471,48 +581,53 @@ static void
 _taskFin(const dispatch_task pTask, void * pContext, void * pReturn) {
   test_context * pCtx = (test_context *)pContext;
   if (pCtx) {
-    pCtx->m_end++;
+    /* update end counter */
+    pCtx->m_end++;    
+    /* signal task end */
+    pthread_mutex_lock(&pCtx->m_endLock);
+    pCtx->m_state = TSTTASK_DONE;
+    pthread_cond_signal(&pCtx->m_endCond);
+    pthread_mutex_unlock(&pCtx->m_endLock);
   }
-  pthread_mutex_lock(&gCancelLock);
-  pthread_cond_signal(&gCancelCond);
-  pthread_mutex_unlock(&gCancelLock);  
 }
 /* ------------ */
 static void
 _taskCnl(const dispatch_task pTask, void * pContext) {
-  pthread_mutex_lock(&gCancelLock);
-  gCanceled++;
-  pthread_cond_signal(&gCancelCond);
-  pthread_mutex_unlock(&gCancelLock);
+  test_context * pCtx = (test_context *)pContext;
+  if (pCtx) {
+    /* update cancel counter */
+    pCtx->m_cancel++;
+    /* signal task canceled */
+    pthread_mutex_lock(&pCtx->m_cancelLock);
+    pCtx->m_state = TSTTASK_CANCEL;
+    pthread_cond_signal(&pCtx->m_cancelCond);
+    pthread_mutex_unlock(&pCtx->m_cancelLock);
+  }
 }
 /* ------------ */
 static void *
 _taskRunReschedule(dispatch_task pTask, void * pContext) {
-  test_context * pCtx = (test_context *)pContext;
-  pCtx->m_run++;
+  _taskRun(pTask, pContext);
   CU_ASSERT(DISPATCH_INVALID_OPERATION == dispatch_schedule(pTask));
-  return pCtx;
+  return pContext;
 }
 /* ------------ */
 static void
 _taskFinReschedule(dispatch_task pTask, void * pContext, void * pReturn) {
-  test_context * pCtx = (test_context *)pContext;
-  pCtx->m_end++;
+  _taskFin(pTask, pContext, pReturn);
   CU_ASSERT(DISPATCH_INVALID_OPERATION == dispatch_schedule(pTask));
 }
 /* ------------ */
 static void *
 _taskRunWait(dispatch_task pTask, void * pContext) {
-  test_context * pCtx = (test_context *)pContext;
-  pCtx->m_run++;
+  _taskRun(pTask, pContext);
   CU_ASSERT(DISPATCH_INVALID_OPERATION == dispatch_wait(&pTask, 1));
-  return pCtx;
+  return pContext;
 }
 /* ------------ */
 static void
 _taskFinWait(dispatch_task pTask, void * pContext, void * pReturn) {
-  test_context * pCtx = (test_context *)pContext;
-  pCtx->m_end++;
+  _taskFin(pTask, pContext, pReturn);
   CU_ASSERT(DISPATCH_INVALID_OPERATION == dispatch_wait(&pTask, 1));
 }
 /* ------------ */
